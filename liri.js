84b5411c82eaf4ee32735681fc48f4ab5b57fec3
solomon <solomonghebrelibanos@gmail.com>
2019-06-03 12:23:53 -0700

    added some features

--- liri.js
+++ liri.js
@@ -19,3 +19,34 @@ let bandsintown = (keys.bandsintown);
 //TAKE USERS COMMAND AND INPUT
 let userInput = process.argv[2];
 let userQuery = procss.argv.slice[3].join('');
+
+//APP LOGIC
+function usercommand(userInput, userQuery){
+    //Make a decesion based on the command
+    switch(userInput){
+        case  "concert-this":
+        concertThis();
+        break;
+        case "spotify-this":
+            spotifyThisSong();
+            break;
+            case "movie-this":
+            movieThis();
+            break;
+            case "do-this":
+                doThis(userQuery);
+                break;
+                default:
+                    console.log("I don't understand");
+                    break;
+    }
+
+}
+usercommand(userInput, userQuery);
+
+function concertThis(){
+
+}
+function spotifyThisSong(){
+
+}
